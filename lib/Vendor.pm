26ce6cbc24eda264e0f6ef1872e7382735d82da3
brian d foy <brian.d.foy@gmail.com>
2006-05-18 23:55:55 +0000

    * cleanups to distro for kwalitee
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1818 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Vendor.pm
+++ lib/Vendor.pm
@@ -278,7 +278,7 @@ brian d foy C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2004-2005, brian d foy, All Rights Reserved.
+Copyright (c) 2004-2006, brian d foy, All Rights Reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
