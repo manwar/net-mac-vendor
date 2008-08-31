d6dfc2f8940221ec7a4ba65e79f6b59ce9f2bfdd
brian d foy <brian@stonehenge.com>
2008-08-31 10:20:59 -0500

    * Add reference to Github

--- lib/Vendor.pm
+++ lib/Vendor.pm
@@ -346,13 +346,9 @@ L<Net::MacMap>
 
 =head1 SOURCE AVAILABILITY
 
-This source is part of a SourceForge project which always has the
-latest sources in CVS, as well as all of the previous releases.
+The source is in Github:
 
-	http://sourceforge.net/projects/brian-d-foy/
-
-If, for some reason, I disappear from the world, one of the other
-members of the project can shepherd this module appropriately.
+	git://github.com/briandfoy/net--mac--vendor.git
 
 =head1 AUTHOR
 
@@ -360,7 +356,7 @@ brian d foy C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2004-2007 brian d foy.  All rights reserved.
+Copyright (c) 2004-2008 brian d foy.  All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
