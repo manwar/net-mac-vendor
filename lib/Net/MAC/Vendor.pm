1b867794c3efb452e387c3c073140f70fb3b9f89
brian d foy <brian.d.foy@gmail.com>
2012-09-03 14:47:35 -0500

    Doc adjustments, some markup, one space after sentences

--- lib/Net/MAC/Vendor.pm
+++ lib/Net/MAC/Vendor.pm
@@ -16,7 +16,7 @@ Net::MAC::Vendor - look up the vendor for a MAC
 	my $array = Net::MAC::Vendor::lookup( $mac );
 
 You can also run this as a script with as many arguments as you
-like.  The module realizes it is a script, looks up the information
+like. The module realizes it is a script, looks up the information
 for each MAC, and outputs it.
 
 	perl Net/MAC/Vendor.pm 00:0d:93:29:f6:c2 00:0d:93:29:f6:c5
@@ -25,24 +25,24 @@ for each MAC, and outputs it.
 
 The Institute of Electrical and Electronics Engineers (IEEE) assigns
 an Organizational Unique Identifier (OUI) to manufacturers of network
-interfaces.  Each interface has a Media Access Control (MAC) address
-of six bytes.  The first three bytes are the OUI.
+interfaces. Each interface has a Media Access Control (MAC) address
+of six bytes. The first three bytes are the OUI.
 
 This module allows you to take a MAC address and turn it into the OUI
-and vendor information.  You can, for instance, scan a network,
-collect MAC addresses, and turn those addresses into vendors.  With
+and vendor information. You can, for instance, scan a network,
+collect MAC addresses, and turn those addresses into vendors. With
 vendor information, you can often guess at what what you are looking
 at (e.g. an Apple product).
 
 You can use this as a module as its individual functions, or call it
-as a script with a list of MAC addresses as arguments.  The module can
+as a script with a list of MAC addresses as arguments. The module can
 figure it out.
 
 This module tries to persistently cache with DBM::Deep the OUI
-information so it can avoid using the network.  If it cannot load
+information so it can avoid using the network. If it cannot load
 DBM::Deep, it uses a normal hash (which is lost when the process
-finishes).  You can preload this cache with the load_cache() function.
- So far, the module looks in the current working directory for a file
+finishes). You can preload this cache with the C<load_cache()> function.
+So far, the module looks in the current working directory for a file
 named mac_oui.db to find the cache. I need to come up with a way to
 let the user set that location.
 
@@ -84,13 +84,13 @@ our $VERSION = 1.19_01;
 =item run( @macs )
 
 If I call this module as a script, this class method automatically
-runs.  It takes the MAC addresses and prints the registered vendor
+runs. It takes the MAC addresses and prints the registered vendor
 information for each address. I can pass it a list of MAC addresses
-and run() processes each one of them.  It prints out what it
+and run() processes each one of them. It prints out what it
 discovers.
 
 This method does try to use a cache of OUI to cut down on the
-times it has to access the network.  If the cache is fully
+times it has to access the network. If the cache is fully
 loaded (perhaps using C<load_cache>), it may not even use the
 network at all.
 
@@ -116,7 +116,7 @@ is the vendor name, and the remaining elements are the address
 lines. Different records may have different numbers of lines,
 although the first two should be consistent.
 
-The normalize_mac() function explains the possible formants
+The C<normalize_mac()> function explains the possible formants
 for MAC.
 
 =cut
@@ -134,11 +134,11 @@ sub lookup {
 
 Takes a MAC address and turns it into the form I need to
 send to the IEEE lookup, which is the first six bytes in hex
-separated by hyphens.  For instance, 00:0d:93:29:f6:c2 turns
+separated by hyphens. For instance, 00:0d:93:29:f6:c2 turns
 into 00-0D-93.
 
 The input string can be a separated by colons or hyphens. They
-can omit leading 0's (which might make things look odd).  We
+can omit leading 0's (which might make things look odd). We
 only need the first three bytes
 
 	00:0d:93:29:f6:c2   # usual form
@@ -182,7 +182,7 @@ sub normalize_mac {
 =item fetch_oui( MAC )
 
 Looks up the OUI information on the IEEE website, or uses a cached
-version of it.  Pass it the result of C<normalize_mac()> and you
+version of it. Pass it the result of C<normalize_mac()> and you
 should be fine.
 
 The C<normalize_mac()> function explains the possible formants for
@@ -242,7 +242,7 @@ sub fetch_oui_from_cache {
 
 =item extract_oui_from_html( HTML, OUI )
 
-Gets rid of the HTML around the OUI information.  It may still be
+Gets rid of the HTML around the OUI information. It may still be
 ugly. The HTML is the search results page of the IEEE ouisearch
 lookup.
 
@@ -292,7 +292,7 @@ Takes a string that looks like
 								Cupertino CA 95014
 								UNITED STATES
 
-and turns it into an array of lines.  It discards the first
+and turns it into an array of lines. It discards the first
 line, strips the leading information from the second line,
 and strips the leading whitespace from all of the lines.
 
@@ -315,13 +315,13 @@ sub parse_oui {
 
 Downloads the current list of all OUIs, parses it with C<parse_oui()>,
 and stores it in C<$Cached> anonymous hash keyed by the OUIs (i.e.
-00-0D-93).  The C<fetch_oui()> will use this cache if it exists. The
+00-0D-93). The C<fetch_oui()> will use this cache if it exists. The
 cache is stored as a C<DBM::Deep> file named after the
 C<NET_MAC_VENDOR_CACHE> environment variable, or C<mac_oui.db> by
 default.
 
-By default, this uses C<http://standards.ieee.org/regauth/oui/oui.txt>
-, but given an argument, it tries to use that. To load from a local
+By default, this uses C<http://standards.ieee.org/regauth/oui/oui.txt>, 
+but given an argument, it tries to use that. To load from a local
 file, use the C<file://> scheme.
 
 If C<load_cache> cannot load the data, it issues a warning and returns
