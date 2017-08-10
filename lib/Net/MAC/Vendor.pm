f7a4a736971db5cc53907f0453e8c921b0487ce4
brian d foy <brian.d.foy@gmail.com>
2017-08-10 08:40:22 +0200

    Note Artistic 2.0 license

--- lib/Net/MAC/Vendor.pm
+++ lib/Net/MAC/Vendor.pm
@@ -567,8 +567,9 @@ brian d foy C<< <bdfoy@cpan.org> >>
 
 Copyright © 2004-2015, brian d foy C<< <bdfoy@cpan.org> >>. All rights reserved.
 
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This program is free software; you can redistribute it under the
+Artistic License 2.0. A license file should have come with this
+distribution.
 
 =cut
 
